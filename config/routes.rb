b4e0fc7900a0cdc0c4f93eaca802442126973b81
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 08:10:40 +0000

    [Fix]ルーティング修

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  #####################customer側##################
+  #####################public側##################
   
   #devise
    devise_for :customers, controllers: {
