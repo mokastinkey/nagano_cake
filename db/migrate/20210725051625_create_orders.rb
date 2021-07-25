d7ed8cd1f4c26ee6cfa3414bb5cc78f98b214cdd
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 05:23:25 +0000

    [Update]ordersテーブル_カラム追加

--- db/migrate/20210725051625_create_orders.rb
+++ db/migrate/20210725051625_create_orders.rb
@@ -1,8 +1,17 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-
-      t.timestamps
+      
+      t.integer :customer_id,    null: false, default: ""
+      t.string :postal_code,     null: false, default: ""
+      t.string :address,         null: false, default: ""
+      t.string :name,            null: false, default: ""
+      t.integer :shipping_cost,  null: false, default: ""
+      t.integer :total_payment,  null: false, default: ""
+      t.integer :payment_method, null: false, default: 0 
+      t.integer :status,         null: false, default: 0
+      
+      t.timestamps null: false
     end
   end
 end
