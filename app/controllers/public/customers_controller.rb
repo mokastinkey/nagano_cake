f69986556580dabf4f01a0aa6481b74b2cab404b
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 07:05:35 +0000

    [New]publish側コントローラとビューの作成

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,13 @@
+class Public::CustomersController < ApplicationController
+  def edit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Rails.application.routes.draw do
   
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confilm'
+    get 'orders/complete'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   namespace :admin do
     resources :order_details, only: [:update]
   end
