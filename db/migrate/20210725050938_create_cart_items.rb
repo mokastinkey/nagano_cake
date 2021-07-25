a675c7ce799e67616a20b366eb84e3e18845b84f
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 05:14:36 +0000

    Update]cart_itemsテーブル_カラム追加

--- db/migrate/20210725050938_create_cart_items.rb
+++ db/migrate/20210725050938_create_cart_items.rb
@@ -1,8 +1,12 @@
 class CreateCartItems < ActiveRecord::Migration[5.2]
   def change
     create_table :cart_items do |t|
+      
+      t.integer :item_id, null: false, default: ""
+      t.integer :customer_id, null: false, default: ""
+      t.integer :amount, null: false, default: ""
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
