dbb2db8ac2c0e2c79ad1326dbcd4263b583e6e0f
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 05:30:38 +0000

    [Update]cart_itemsテーブル_カラム作成

--- db/migrate/20210725052659_create_order_details.rb
+++ db/migrate/20210725052659_create_order_details.rb
@@ -1,8 +1,14 @@
 class CreateOrderDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :order_details do |t|
-
-      t.timestamps
+      
+      t.integer :order_id,      null: false, default: ""
+      t.integer :item_id,       null: false, default: ""
+      t.integer :price,         null: false, default: ""
+      t.integer :amount,        null: false, default: ""
+      t.integer :making_status, null: false, default: 0
+    
+      t.timestamps null: false
     end
   end
 end
