095f2debd87a3e5b3ef438e79ffcdc693536d27c
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 05:25:20 +0000

    [New]ordersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_050938) do
+ActiveRecord::Schema.define(version: 2021_07_25_051625) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -74,6 +74,19 @@ ActiveRecord::Schema.define(version: 2021_07_25_050938) do
     t.text "introduction", default: "", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "name", default: "", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
