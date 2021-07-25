89f2b273c3b81aa2ad871e0b0d0cee946486b006
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 10:56:43 +0000

    [Add]enum追加

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,13 @@ class OrderDetail < ApplicationRecord
   
   belongs_to :item
   belongs_to :order
+  
+  enum status: {
+    入金待ち: 0,
+　　入金確認: 1, 
+    制作中: 2,
+    発送準備: 3,
+    発送済み: 4
+  }
+  
 end
