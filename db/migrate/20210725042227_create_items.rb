eec4a1e70a352c1e57b6004edbcb7e95cc209549
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 05:01:28 +0000

    [Update]itemテーブル_カラム追加

--- db/migrate/20210725042227_create_items.rb
+++ db/migrate/20210725042227_create_items.rb
@@ -1,8 +1,15 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      
+      t.integer :genre_id,  null: false, default: ""
+      t.string :name,       null: false, default: ""
+      t.string :image_id,   null: false, default: ""
+      t.text :introduction, null: false, default: ""
+      t.integer :price,     null: false, default: ""
+      t.boolean :is_active, null: false, default: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
