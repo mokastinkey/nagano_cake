a23b2699992eee60ff2accfe6a44e0263ba987e8
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 08:07:37 +0000

    [Fix]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,81 @@
 Rails.application.routes.draw do
   
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confilm'
-    get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/edit'
-    get 'customers/update'
-    get 'customers/unsubscribe'
-    get 'customers/withdraw'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :admin do
-    resources :order_details, only: [:update]
-  end
-  namespace :admin do
-    resources :orders, only: [:show, :update]
-  end
-  namespace :admin do
-    resources :customers, only: [:index, :show, :edit, :update]
-  end
-  namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update]
-  end
-  namespace :admin do
-    resources :items, only: [:index, :new, :edit, :create, :show, :update]
-  end
+  #####################customer側##################
   
-  get '/admin' => 'admin/homes#top', as: 'admin_home'
-  
-  devise_for :admins, path: 'admin', controllers: {
-    sessions:      'admin/sessions'
-  }
-  
-  devise_for :customers, controllers: {
+  #devise
+   devise_for :customers, controllers: {
     registrations: 'public/registrations',
     sessions:      'public/sessions'
   }
   
-  root to: 'public#homes'
+  #address
+  scope module: :public do
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  
+  #order
+  scope module: :public do
+    resources :orders, only: [:index, :create, :show, :new]
+  end
+  post '/orders/confirm' => 'public/orders#confirm', as: 'confirm_order'
+  get '/orders/complete' => 'public/orders#complete', as: 'complete_order'
+  
+  #cart_item
+  scope module: :public do
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+  end
+  delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all', as: 'destroy_all_cart_item'
+  
+  #customer
+  scope module: :public do
+    resource :customers, only: [:edit, :update]
+  end
+  get '/customers/unsubscribe' => 'public/customers#unsubscribe', as: 'unsubscribe_customer'
+  patch '/customers/withdraw' => 'public/customers#withdraw', as: 'withdraw_customer'
+  
+  #item
+  scope module: :public do
+    resource :items, only: [:index, :show]
+  end
+  
+  #homes
+  get '/about' => 'public/homes#top', as: 'about_page'
+  
+  root to: 'public/homes#top'
+  
+  ###################################admin側########################
+  
+  #devise
+  devise_for :admins, path: 'admin', controllers: {
+    sessions:      'admin/sessions'
+  }
+  
+  #order_details
+  namespace :admin do
+    resources :order_details, only: [:update]
+  end
+  
+  #order
+  namespace :admin do
+    resources :orders, only: [:show, :update]
+  end
+  
+  #customer
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  
+  #genre
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  
+  #item
+  namespace :admin do
+    resources :items, only: [:index, :new, :edit, :create, :show, :update]
+  end
+  
+  #homes
+  get '/admin' => 'admin/homes#top', as: 'admin_home'
+  
 end
