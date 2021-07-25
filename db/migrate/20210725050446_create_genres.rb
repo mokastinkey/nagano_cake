4bbb26ff2ebea55a776110839934ef62b2a718bd
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-25 05:07:37 +0000

    [Update]genresテーブル_カラム追加

--- db/migrate/20210725050446_create_genres.rb
+++ db/migrate/20210725050446_create_genres.rb
@@ -1,8 +1,10 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
+      
+      t.string :name, null: false, default: ""
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
