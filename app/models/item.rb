89252f11d3ee690e01c1c42ac5c90b857f04fd6b
EC2 Default User <ec2-user@ip-172-31-80-234.ec2.internal>
2021-07-28 05:56:42 +0000

    [Fix]viewファイル、コントローラなど色々修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_details
   belongs_to :genre
+  
+  def add_tax_price
+    (self.price * 1.10).round
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,39 +8,29 @@ Rails.application.routes.draw do
     sessions:      'public/sessions'
   }
   
-  #address
   scope module: :public do
-    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy] #address
+    resources :orders, only: [:index, :create, :show, :new] #order
+    resources :cart_items, only: [:index, :update, :destroy, :create] #cart_item
+    resource :customers, only: [:edit, :update] #customer
+    resource :items, only: [:index, :show, :create] #item
   end
   
   #order
-  scope module: :public do
-    resources :orders, only: [:index, :create, :show, :new]
-  end
   post '/orders/confirm' => 'public/orders#confirm', as: 'confirm_order'
   get '/orders/complete' => 'public/orders#complete', as: 'complete_order'
   
   #cart_item
-  scope module: :public do
-    resources :cart_items, only: [:index, :update, :destroy, :create]
-  end
   delete '/cart_items/destroy_all' => 'public/cart_items#destroy_all', as: 'destroy_all_cart_item'
   
   #customer
-  scope module: :public do
-    resource :customers, only: [:edit, :update]
-  end
   get '/customers/unsubscribe' => 'public/customers#unsubscribe', as: 'unsubscribe_customer'
   patch '/customers/withdraw' => 'public/customers#withdraw', as: 'withdraw_customer'
-  
-  #item
-  scope module: :public do
-    resource :items, only: [:index, :show, :create]
-  end
   
   #homes
   get '/about' => 'public/homes#top', as: 'about_page'
   
+  #root
   root to: 'public/homes#top'
   
   ###################################admin側########################
@@ -50,29 +40,12 @@ Rails.application.routes.draw do
     sessions:      'admin/sessions'
   }
   
-  #order_details
   namespace :admin do
-    resources :order_details, only: [:update]
-  end
-  
-  #order
-  namespace :admin do
-    resources :orders, only: [:show, :update]
-  end
-  
-  #customer
-  namespace :admin do
-    resources :customers, only: [:index, :show, :edit, :update]
-  end
-  
-  #genre
-  namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update]
-  end
-  
-  #item
-  namespace :admin do
-    resources :items, only: [:index, :new, :edit, :create, :show, :update]
+    resources :order_details, only: [:update] #order_details
+    resources :orders, only: [:show, :update] #order
+    resources :customers, only: [:index, :show, :edit, :update] #customer
+    resources :genres, only: [:index, :create, :edit, :update] #genre
+    resources :items, only: [:index, :new, :edit, :create, :show, :update] #item
   end
   
   #homes
